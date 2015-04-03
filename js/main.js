fa0e4e122ebf03911c0e5a6915c1ea2ef0f38c4d
noeleillien <nai2105@columbia.edu>
2015-04-02 20:00:35 -0400

    changes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+(function(){
+	// Start writing your JavaScript inside, and only inside this function
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
+
+}).call(this);
